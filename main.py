cb253ec79f62885eea20da7417a5297484e05360
guestuser10 <usuarioguest10@gmail.com>
2025-03-29 10:27:36 -0700

    port

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def create_database():
     try:
         # Conectar a MySQL sin especificar una base de datos
         conn = pymysql.connect(
-            host="127.0.1:3308",
+            host="127.0.0.1:3306",
             user="root",
             password="1234"
         )
